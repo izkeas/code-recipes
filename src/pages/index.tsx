275bac08cc8484151194d9055956f917f91228b1
izkeas <izkeasyomito@gmail.com>
2023-03-23 15:29:24 -0300

    Changed index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,56 @@
-import ProjectList from "@/components/ProjectsList";
-import projects from "@/tempData/projects";
-import { Container } from "@mui/system";
+import { Button, Container, Grid, Stack, Typography } from "@mui/material";
+import LogoSvg from "@/components/logoIcon";
+import LogoText from "@/components/LogoText";
 
-export default function Home() {
-  return (
-    <>
-      <Container>
-        <ProjectList projects={projects}/>
-      </Container>
-    </>
-  )
+
+export default function Index(){
+
+    return (
+        <Container>
+            <Grid container>
+
+                <Grid item xs={8}>
+                    <Container>
+                        <div
+                            style={{
+                                margin : "auto",
+                                width : "400px"
+                            }}
+                        >
+                            <LogoSvg/>
+                            <LogoText/>
+                        </div>
+                    </Container>
+                </Grid>
+
+                <Grid item>
+                    <Stack  justifyContent={"center"}
+                        style={{
+                            height : "100%",
+                            margin : "auto"
+                        }}
+                    >
+                        <Button>
+                            <Typography variant={"h4"}>
+                                Algorithms
+                            </Typography>
+                        </Button>
+                        
+                        <Button>
+                            <Typography variant={"h4"}>
+                                Templates
+                            </Typography>
+                        </Button>
+
+                        <Button>
+                            <Typography variant={"h4"}>
+                                Design Patterns
+                            </Typography>
+                        </Button>
+
+                    </Stack>
+                </Grid>
+            </Grid>
+        </Container>
+    )
 }
