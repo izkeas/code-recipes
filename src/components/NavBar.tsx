22c4549ef8a9c6bef7f54b1f7d411631dad62d87
izkeas <izkeasyomito@gmail.com>
2023-03-23 15:32:41 -0300

    Added logo to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,6 @@
-import { AppBar, Toolbar, Typography, Container } from "@mui/material";
+import { AppBar, Toolbar, Typography, Container, Stack, Button, Grid } from "@mui/material";
 import Link from "next/link";
-
+import LogoSvg from "./logoIcon";
 
 export default function NavBar() {
 
@@ -14,15 +14,68 @@ export default function NavBar() {
 
             elevation={0}
         >
-            <Container>
+
                 <Toolbar>
-                    <Link href={"/"}>
-                        <Typography variant="h3">
-                            Programming Recipes
-                        </Typography>
-                    </Link>
+                    <Container>
+                        <Grid container>
+
+                            <Grid item xs={8}>
+                                <Stack direction={"row"}>
+                                    <LogoSvg 
+                                            style={{
+                                                width : "50px",
+                                                height : "50px"
+                                            }}
+                                        />
+
+                                    <Typography variant="h3"
+                                        style={{
+                                            marginTop : "auto",
+                                            marginBottom : "auto"
+                                        }}
+                                    >
+                                        Code Recipes
+                                    </Typography>
+                                </Stack>
+                            </Grid>
+
+                            {/* PAges list only desktop */}
+                            <Grid item 
+                                sx={{
+                                    display : {xs : "none", md : "block"}
+                                }}
+                            >
+                                <Link href={"/projects"}>
+                                    <Button>
+                                        Projects
+                                    </Button>
+                                </Link>
+                            </Grid>
+
+
+                            {/* Menu only phone*/}
+                            <Grid item xs={4}
+
+                                sx={{
+                                    display : {xs : "flex", md : "none"},
+                                    flexDirection : "row-reverse",
+                                    alignContent : "flex-end",
+                                    alignItems : "flex-end"
+                                }}
+
+                            >
+                                <Link href={"/projects"}>
+                                    <Button>
+                                        Menu
+                                    </Button>
+                                </Link>
+                            </Grid>
+
+                        </Grid>
+
+                    </Container>
+
                 </Toolbar>
-            </Container>
         </AppBar>
     )
 }
