5bd27b76461e891d2b9584d12841f813113525fc
izkeas <izkeasyomito@gmail.com>
2023-03-29 16:33:21 -0300

    Added Project Component; removed projectPage component

---
+++ src/components/Project.tsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from "react";
+import { Button, Stack, Typography, Grid, Container, TextField } from "@mui/material";
+
+import reactStrToComponent from "@/utils/ReactStrToComponent";
+
+export default function Project(props : {code : string}){
+    const [ProjectComponent, setProjectComponent] = useState<() => JSX.Element>();
+
+    useEffect(() => {
+        const component = reactStrToComponent(props.code, React, { Typography, Button, Stack, Grid, Container, TextField});
+        setProjectComponent(component);
+    
+    }, [props.code])
+
+    return (
+        <div>
+            { ProjectComponent && (
+                <ProjectComponent/>
+            )}
+        </div>
+    )
+}
--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -1,8 +1,14 @@
-import ProjectPage from "@/components/ProjectPage";
-import { Container } from "@mui/material";
+import { Container, Paper, Stack, Typography } from "@mui/material";
 import { useRouter } from "next/router";
-import projects, { ProjectObj } from "@/tempData/projects";
+import { ProjectObj } from "@/tempData/projects";
 import { useEffect, useState } from "react";
+
+import { ProjectWindow } from "@/components/ProjectWindow";
+import Wrapper from "@/components/Wrapper";
+import Project from "@/components/Project";
+
+import { CopyBlock, atomOneDark as codeTheme  } from "react-code-blocks";
+
 
 async function getProjectFromName(name : string){
     const request = await fetch("/api/project", {
@@ -18,7 +24,11 @@ async function getProjectFromName(name : string){
     return result;
 }
 
-export default function Project(){
+interface Props {
+    project : ProjectObj;
+}
+
+export default function ProjectPage(){
     const router = useRouter();
     const {id } = router.query;
     const [project, setProject ] = useState<ProjectObj>();
@@ -36,7 +46,115 @@ export default function Project(){
     return (
         <Container>
             {project && (
-                <ProjectPage project={project}/>
+                <Paper
+                elevation={0}
+                >
+                <Wrapper>
+                    <Stack spacing={2}>
+
+
+                        <ProjectWindow>
+                            <Project code={project.code}/>
+                        </ProjectWindow>
+
+                        <Typography variant={"h2"} fontWeight={700}>
+                            {project.name}
+                        </Typography>
+                        
+                        <Typography>
+                            {project.description}
+                        </Typography>
+
+                        { project.useCases && project.useCases.length > 0 && 
+                            (
+                                <Typography variant={"h3"} fontWeight={700}>
+                                    Use Cases
+                                </Typography>
+                            )
+                        }
+
+                        <ul>
+                            { 
+                                project.useCases && 
+                                project.useCases.length > 0 && 
+                                project.useCases.map( (useCase) => (
+                                    <li key={useCase}>
+                                        {useCase}
+                                    </li>
+                                ))
+                            }
+                        
+                        </ul>
+
+                        <Typography variant={"h3"} fontWeight={700}>
+                            Implementation Steps
+                        </Typography>
+
+                        <ul>
+                            { project.implementationSteps.map( (step, index) => (
+                                <li key={index}>{step}</li>
+                            ))}
+                        </ul>
+
+                        { project.algorithmImplementation && (
+                            <>
+                                <Typography variant={"h3"} fontWeight={700}>
+                                    Algorithm Code Example
+                                </Typography>
+
+                                <CopyBlock
+                                    text={project.algorithmImplementation}
+                                    language={"typescript"}
+                                    wrapLines
+                                    theme={codeTheme}
+                                />
+                            </>
+                        )}
+
+
+                        { project.code && (
+                            <>
+                                <Typography variant={"h3"} fontWeight={700}>
+                                    React Implementation
+                                </Typography>
+
+                                <CopyBlock
+                                    text={project.code}
+                                    language={"typescript"}
+                                    wrapLines
+                                    theme={codeTheme}
+                                />
+                            </>
+                        )}
+
+
+                        { project.references && project.references.length > 0 && 
+                            (
+                                <Typography variant={"h2"}>
+                                    Use Cases
+                                </Typography>
+                            )
+                        }
+
+                        <ul
+                        >
+                            { 
+                                project.references && 
+                                project.references.length > 0 && 
+                                project.references.map( (useCase) => (
+                                    <li key={useCase}>
+                                        <a href={useCase}>{useCase}</a>
+                                    </li>
+                                ))
+                            }
+                        
+                        </ul>
+
+                    </Stack>
+
+                </Wrapper>
+                </Paper>
+
             )}
 
             { project === null && (
