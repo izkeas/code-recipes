63d41f81883a4bf4347a5f8270be23ea956a6a5f
izkeas <izkeasyomito@gmail.com>
2023-03-31 11:10:24 -0300

    Added env test log

--- src/pages/api/project.ts
+++ src/pages/api/project.ts
@@ -5,6 +5,7 @@ export default async function getProject(req : NextApiRequest, res: NextApiRespo
     
     if (req.method === "POST"){
 
+        console.log(process.env.MONGODB_URI);
 
         const client = new MongoClient(process.env.MONGODB_URI || "");
         await client.connect();
