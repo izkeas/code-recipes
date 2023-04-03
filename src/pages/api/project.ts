553edaba135367cae7b20b41601454d05e359d08
izkeas <izkeasyomito@gmail.com>
2023-04-03 16:20:47 -0300

    Revert "Added env test log"
    This reverts commit 63d41f81883a4bf4347a5f8270be23ea956a6a5f.

--- src/pages/api/project.ts
+++ src/pages/api/project.ts
@@ -5,7 +5,6 @@ export default async function getProject(req : NextApiRequest, res: NextApiRespo
     
     if (req.method === "POST"){
 
-        console.log(process.env.MONGODB_URI);
 
         const client = new MongoClient(process.env.MONGODB_URI || "");
         await client.connect();
